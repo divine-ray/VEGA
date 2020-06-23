1dea8447d458dcf8757f9d27c2eabf62ce5c2b0d
Ludima <luedde.mahr@web.de>
2020-06-23 17:16:59 +0200

    Update downloads.js

--- commands/downloads.js
+++ commands/downloads.js
@@ -2,27 +2,34 @@
 // :)
 
 module.exports = {
-    const http = require("http");
+    name: 'avatar',
+    cooldown: 5,
+    aliases: ['icon', 'pfp', 'pp'],
+    description: 'Prints a link to the Profile Picture of the mentioned User(s)',
+    
+    execute(message) {
+        const http = require("http");
 
-    http.get('http://xj4kuswsas2jjggi.myfritz.net:8080/downloadtext', (res) => {
-        let data = '';
+        http.get('http://xj4kuswsas2jjggi.myfritz.net:8080/downloadtext', (res) => {
+            let data = '';
 
-        res.on('data', (chunk) => {
-            data += chunk;
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            resp.on('end', () => {
+                var list = data.split(";");
+                const downloadEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Momentan verfügbare Dateien:')
+                    .setURL('http://xj4kuswsas2jjggi.myfritz.net:8080/download/')
+                    for (let step = 0; step < list.length-1; step++) {
+                        .addField(list[step])
+                    }
+                    .setTimestamp()
+                    .setFooter('Die Verfügbarkeit der Website wird nicht gewährleistet.');
+                channel.send(downloadEmbed);
+            }
         });
-
-        resp.on('end', () => {
-            var list = data.split(";");
-            const downloadEmbed = new Discord.MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('Momentan verfügbare Dateien:')
-                .setURL('http://xj4kuswsas2jjggi.myfritz.net:8080/download/')
-                for (let step = 0; step < list.length-1; step++) {
-                    .addField(list[step])
-                }
-                .setTimestamp()
-                .setFooter('Die Verfügbarkeit der Website wird nicht gewährleistet.');
-            channel.send(downloadEmbed);
-        }
-    });
+    }
 }
