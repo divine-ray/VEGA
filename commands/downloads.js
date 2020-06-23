535274a41129c96dc240dc98e7fe214fd2179e78
Ludima <luedde.mahr@web.de>
2020-06-23 17:14:56 +0200

    Update downloads.js

--- commands/downloads.js
+++ commands/downloads.js
@@ -1,2 +1,28 @@
 //add lüdde integation
 // :)
+
+module.exports = {
+    const http = require("http");
+
+    http.get('http://xj4kuswsas2jjggi.myfritz.net:8080/downloadtext', (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            var list = data.split(";");
+            const downloadEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Momentan verfügbare Dateien:')
+                .setURL('http://xj4kuswsas2jjggi.myfritz.net:8080/download/')
+                for (let step = 0; step < list.length-1; step++) {
+                    .addField(list[step])
+                }
+                .setTimestamp()
+                .setFooter('Die Verfügbarkeit der Website wird nicht gewährleistet.');
+            channel.send(downloadEmbed);
+        }
+    });
+}
