d29b1677bb9437de888fd7ab1a6779b70afb6691
Ludima <luedde.mahr@web.de>
2020-06-24 12:02:04 +0200

    Update downloads.js

--- commands/downloads.js
+++ commands/downloads.js
@@ -1,8 +1,5 @@
 //add lüdde integation
 // :)
-
-const Discord = require('discord.js');
-
 module.exports = {
     name: 'downloads',
     cooldown: 5,
@@ -18,9 +15,26 @@ module.exports = {
                 data += chunk;
             });
 
-            res.on('end', () => {          
+            res.on('end', () => {
+                var values = [];
+                for (let i = 0; i < data.split(";")-2; i++) {
+                    values += { value: data.split(";")[i] };
+                }
+                
+                const downloadEmbed = Discord.MessageEmbed();
+                downloadEmbed.setColor("#6e3684");
+                downloadEmbed.setTitle("Downloadpage");
+                downloadEmbed.setURL("http://xj4kuswsas2jjggi.myfritz.net:8080/download/");
+                downloadEmbed.setDescription("Eine Liste aller dämonischen Dateien.");
+                
+                
+                
+                downloadEmbed.setTimestamp();
+                downloadEmbed.setFooter("Die Erreichbarkeit der Website wird nicht gewährleistet.");
+                
                 message.channel.send(data);
             });
+            
         });
     }
 }
