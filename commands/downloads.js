fb9d6d79193881774436fd13b269d11705a5df31
CreepTV <spiritofst.minecraft@gmail.com>
2020-06-24 11:46:39 +0200

    DOwnloads command, wip

--- commands/downloads.js
+++ commands/downloads.js
@@ -21,7 +21,7 @@ module.exports = {
             res.on('end', () => {
                 var list = data.split(";");
                 var list_2 = [];
-                for (let i = 0; i < list.length-1; i++) {
+                for (let i = 0; i < list.length - 1; i++) {
                     list_2 += { value: list[i] }
                 }
                 const downloadEmbed = new Discord.MessageEmbed()
@@ -29,9 +29,10 @@ module.exports = {
                     .setTitle('Momentan verfügbare Dateien:')
                     .setURL('http://xj4kuswsas2jjggi.myfritz.net:8080/download/')
                     .addFields(list_2);
-                
-              
+
+
                 message.channel.send(downloadEmbed);
-        });
-    }
+            });
+        }
+}
 }
