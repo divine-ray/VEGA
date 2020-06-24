e35932e1155e6b65b508d56c78ddd208d32fb942
Ludima <luedde.mahr@web.de>
2020-06-24 12:42:21 +0200

    Update downloads.js

--- commands/downloads.js
+++ commands/downloads.js
@@ -1,45 +1,5 @@
 //add lüdde integation
 // :)
-/*const Discord = require("Discord.js");
-
-module.exports = {
-    name: 'downloads',
-    aliases: ['dl', 'download'],
-    cooldown: 5,
-    description: 'Prints all available Files from our download site.',
-
-    execute(message) {
-        const http = require("http");
-
-        http.get('http://xj4kuswsas2jjggi.myfritz.net:8080/downloadtext', (res) => {
-            let data = '';
-
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            res.on('end', () => {
-
-                const downloadEmbed = new Discord.MessageEmbed();
-                downloadEmbed.setColor("#6e3684");
-                downloadEmbed.setTitle("Downloadpage");
-                downloadEmbed.setURL("http://xj4kuswsas2jjggi.myfritz.net:8080/download/");
-                downloadEmbed.setDescription("Eine Liste aller Dateien.");
-
-
-
-                downloadEmbed.setTimestamp();
-                downloadEmbed.setFooter("Die Erreichbarkeit der Website wird nicht gewährleistet.");
-
-                message.channel.send(data);
-            });
-
-        });
-    }
-}*/
-
-//add lüdde integation
-// :)
 const Discord = require("Discord.js");
 
 module.exports = {
