45fa2836be8fb34fdfca75108ece64bd5e961813
CreepTV <spiritofst.minecraft@gmail.com>
2020-06-24 12:59:27 +0200

    FIxed dl aliases

--- commands/downloads.js
+++ commands/downloads.js
@@ -5,37 +5,39 @@ const Discord = require("Discord.js");
 module.exports = {
     name: 'downloads',
     cooldown: 5,
+    aliases: ['dl', 'download'],
     description: 'Prints all available Files.',
 
     execute(message) {
         const http = require("http");
 
         http.get('http://xj4kuswsas2jjggi.myfritz.net:8080/downloadtext', (res) => {
-            let data = '';
+            let string = '';
 
             res.on('data', (chunk) => {
-                data += chunk;
+                string += chunk;
             });
 
             res.on('end', () => {
+                var data = JSON.parse(string);
                 const downloadEmbed = new Discord.MessageEmbed();
                 downloadEmbed.setColor("#6e3684");
                 downloadEmbed.setTitle("Downloadpage");
                 downloadEmbed.setURL("http://xj4kuswsas2jjggi.myfritz.net:8080/download/");
                 downloadEmbed.setDescription("Eine Liste aller dämonischen Dateien.");
 
-                for (let i = 0; i < data.length - 1; i++) {
+                /*for (let i = 0; i < data.length-1; i++) {
                     downloadEmbed.addField("Benutzer: ", data[i]["name"]);
-                    for (let j = 0; j < data[i]["files"].length - 1; j++) {
-                        downloadEmbed.addField(" " + data[i]["files"][j]["file"] + ": ", data[i]["files"][j]["url"]);
+                    for (let j = 0; j < data[i]["files"].length-1; j++) {
+                        downloadEmbed.addField(" "+data[i]["files"][j]["file"]+": ", data[i]["files"][j]["url"]);
                     }
-                }
+                }*/
 
 
                 downloadEmbed.setTimestamp();
                 downloadEmbed.setFooter("Die Erreichbarkeit der Website wird nicht gewährleistet.");
 
-                message.channel.send(downloadEmbed);
+                message.channel.send(data);
             });
 
         });
