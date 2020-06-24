e13272eca4b09cb74592b9eab3b49445984df943
CreepTV <spiritofst.minecraft@gmail.com>
2020-06-24 11:22:27 +0200

    Added a borked version of d/help

--- commands/downloads.js
+++ commands/downloads.js
@@ -1,10 +1,10 @@
 //add lüdde integation
 // :)
-/*
+
 module.exports = {
     name: 'downloads',
     cooldown: 5,
-    description: 'Prints all available Files.'
+    description: 'Prints all available Files.',
 
     execute(message) {
         const http = require("http");
@@ -22,14 +22,14 @@ module.exports = {
                     .setColor('#0099ff')
                     .setTitle('Momentan verfügbare Dateien:')
                     .setURL('http://xj4kuswsas2jjggi.myfritz.net:8080/download/')
-                    for (let step = 0; step < list.length-1; step++) {
-                        .addField(list[step])
-                    }
-                    .setTimestamp()
+                for (let step = 0; step < list.length - 1; step++) {
+                    addField(list[step])
+                }
+                setTimestamp()
                     .setFooter('Die Verfügbarkeit der Website wird nicht gewährleistet.');
                 message.channel.send(downloadEmbed);
-            }
-        });
+            })
+        })
     }
+
 }
-*/
