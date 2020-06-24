50e4cc0fd0d8debd8a5ff230e048392aeac4ec47
Ludima <luedde.mahr@web.de>
2020-06-24 12:27:41 +0200

    Update downloads.js

--- commands/downloads.js
+++ commands/downloads.js
@@ -1,6 +1,6 @@
 //add lüdde integation
 // :)
-const Discord = require("Discord.js");
+/*const Discord = require("Discord.js");
 
 module.exports = {
     name: 'downloads',
@@ -26,6 +26,50 @@ module.exports = {
                 downloadEmbed.setDescription("Eine Liste aller dämonischen Dateien.");
 
 
+
+                downloadEmbed.setTimestamp();
+                downloadEmbed.setFooter("Die Erreichbarkeit der Website wird nicht gewährleistet.");
+
+                message.channel.send(data);
+            });
+
+        });
+    }
+}*/
+
+//add lüdde integation
+// :)
+const Discord = require("Discord.js");
+
+module.exports = {
+    name: 'downloads',
+    cooldown: 5,
+    description: 'Prints all available Files.',
+
+    execute(message) {
+        const http = require("http");
+
+        http.get('http://xj4kuswsas2jjggi.myfritz.net:8080/downloadtext', (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {                
+                const downloadEmbed = new Discord.MessageEmbed();
+                downloadEmbed.setColor("#6e3684");
+                downloadEmbed.setTitle("Downloadpage");
+                downloadEmbed.setURL("http://xj4kuswsas2jjggi.myfritz.net:8080/download/");
+                downloadEmbed.setDescription("Eine Liste aller dämonischen Dateien.");
+                
+                for (let i = 0; i < data.length-1; i++) {
+                    downloadEmbed.addField("Benutzer: ", data[i]["name"]);
+                    for (let j = 0; j < data[i]["files"].length-1; j++) {
+                        downloadEmbed.addField(" "+data[i]["files"][j]["file"]+": ", data[i]["files"][j]["url"])
+                    }
+                }
+                
 
                 downloadEmbed.setTimestamp();
                 downloadEmbed.setFooter("Die Erreichbarkeit der Website wird nicht gewährleistet.");
