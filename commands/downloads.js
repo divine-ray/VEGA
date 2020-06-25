84f5f4e596d7fcc89453be76968c254bfa97a1c3
CreepTV <spiritofst.minecraft@gmail.com>
2020-06-25 12:27:45 +0200

    finally, the d/downloads command provides a proper embed

--- commands/downloads.js
+++ commands/downloads.js
@@ -1,6 +1,6 @@
 //add lüdde integation
 // :)
-const Discord = require("Discord.js");
+const Discord = require("discord.js");
 
 module.exports = {
     name: 'downloads',
@@ -26,75 +26,38 @@ module.exports = {
             string += chunk;
         });
 
-        let response = await new Promise(resolve => {
+        const response = await new Promise(resolve => {
             res.on('end', () => {
                 var data = JSON.parse(string);
 
                 console.log('has data %j', data);
 
-                // const downloadEmbed = new Discord.MessageEmbed();
-                // downloadEmbed.setColor("#6e3684");
-                // downloadEmbed.setTitle("Downloadpage");
-                // downloadEmbed.setURL("http://xj4kuswsas2jjggi.myfritz.net:8080/download/");
-                // downloadEmbed.setDescription("Eine Liste aller dämonischen Dateien.");
+                const downloadEmbed = new Discord.MessageEmbed();
+                downloadEmbed.setColor("#6e3684");
+                downloadEmbed.setTitle("Downloadpage");
+                downloadEmbed.setURL("http://xj4kuswsas2jjggi.myfritz.net:8080/download/");
+                downloadEmbed.setDescription("Eine Liste aller dämonischen Dateien.");
 
 
-                // for (let i = 0; i < data.length - 1; i++) {
-                //     downloadEmbed.addField("Benutzer: ", data[i]["name"]);
-                //     for (let j = 0; j < data[i]["files"].length - 1; j++) {
-                //         downloadEmbed.addField(" " + data[i]["files"][j]["file"] + ": ", data[i]["files"][j]["url"]);
-                //     }
-                // }
+                for (let i = 0; i < data.length; i++) {
+                    downloadEmbed.addField("Benutzer: ", data[i]["name"]);
+                    for (let j = 0; j < data[i]["files"].length; j++) {
+                        downloadEmbed.addField(" " + data[i]["files"][j]["file"] + ": ", data[i]["files"][j]["url"]);
+                    }
+                }
 
 
 
-                // downloadEmbed.setTimestamp();
-                // downloadEmbed.setFooter("Die Erreichbarkeit der Website wird nicht gewährleistet.");
+                downloadEmbed.setTimestamp();
+                downloadEmbed.setFooter("Die Erreichbarkeit der Website wird nicht gewährleistet.");
 
-                // console.log(downloadEmbed);
+                console.log(downloadEmbed);
 
-                const downloadEmbed = new Discord.MessageEmbed()
-                    .setColor('#0099ff')
-                    .setTitle('Download List')
-                    .setURL('http://xj4kuswsas2jjggi.myfritz.net:8080/downloadtext')
-                    .setAuthor('Ludima',// '', ''//
-                    )
-                    .setDescription('All available Files of our downloads site')
-                    // .setThumbnail('')
-                    // .addFields(
-                    //     { name: 'Regular field title', value: 'Some value here' },
-                    //     { name: '\u200B', value: '\u200B' },
-                    //     { name: 'Inline field title', value: 'Some value here', inline: true },
-                    //     { name: 'Inline field title', value: 'Some value here', inline: true },
-                    // )
-                    // .addField('Inline field title', 'Some value here', true)
-                    // .setImage('')
-                    .setTimestamp()
-                    .setFooter('Some footer text here', '');
 
                 //message.channel.send(JSON.stringify(data, null, 2));
                 resolve(downloadEmbed);
             });
         })
-
-        response = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Download List')
-            .setURL('http://xj4kuswsas2jjggi.myfritz.net:8080/downloadtext')
-            .setAuthor('Ludima',// '', ''//
-            )
-            .setDescription('All available Files of our downloads site')
-            .setThumbnail('')
-            .addFields(
-                { name: 'Regular field title', value: 'Some value here' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-            )
-            .addField('Inline field title', 'Some value here', true)
-            .setImage('')
-            .setTimestamp()
-            .setFooter('Some footer text here', '');
 
         console.log(response);
 
