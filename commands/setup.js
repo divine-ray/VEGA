2f58bca82cc9306eb283c73708c01420d4866aa9
CreepTV <spiritofst.minecraft@gmail.com>
2020-06-26 12:50:52 +0200

    d/setup command works (somewhat), misc stuff..?

--- commands/setup.js
+++ commands/setup.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 module.exports = {
     name: 'setup',
     cooldown: 500,
@@ -6,10 +8,29 @@ module.exports = {
     execute(message, args) {
         message.channel.send('Setting up Roles..');
         console.log('Setting up Roles..')
-        new Discord.Role();
+        message.guild.roles.create({
+            data: {
+                name: 'Vacant (General Assemby)',
+                hexColor: '#e6083b',
+            },
+            reason: 'Needed for the General Assembly',
 
 
-        //#858ed8
+
+
+
+        })
+        message.guild.roles.create({
+            data: {
+                name: 'Present (General Assemby)',
+                hexColor: '#549c2d',
+            },
+            reason: 'Needed for the General Assembly',
+        })
+
+        console.log('Created all Neccesary roles for this guild');
+        message.channel.send('all roles are good to go now.')
+        //
 
 
 
