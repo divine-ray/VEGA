d174dfdfa0d75b2c2f694a895837c7229ca4f73a
CreepTV <lixernispunkt2@gmx.net>
2020-06-29 11:20:32 +0200

    d/setup command now has checking

--- commands/setup.js
+++ commands/setup.js
@@ -4,8 +4,14 @@ module.exports = {
     name: 'setup',
     cooldown: 500,
     guildOnly: true,
+    
+    
+    
+    
+    
     description: 'Creates the required roles for a couple features of this bot.',
     execute(message, args) {
+        var initalized = false
         message.channel.send('Setting up Roles..');
         console.log('Setting up Roles..')
         message.guild.roles.create({
@@ -14,12 +20,8 @@ module.exports = {
                 hexColor: '#e6083b',
             },
             reason: 'Needed for the General Assembly',
-
-
-
-
-
         })
+       
         message.guild.roles.create({
             data: {
                 name: 'Present (General Assemby)',
@@ -28,8 +30,13 @@ module.exports = {
             reason: 'Needed for the General Assembly',
         })
 
+
+        //More roles n' stuff will be here
+
         console.log('Created all Neccesary roles for this guild');
+        initalized = true
         message.channel.send('all roles are good to go now.')
+        console.log(initalized)
         //
 
 
