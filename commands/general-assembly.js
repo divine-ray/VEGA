4f87ed9919aac3d7136b5e76384a5b1bd62ce375
CreepTV <spiritofst.minecraft@gmail.com>
2020-06-30 15:38:13 +0200

    unfinished check

--- commands/general-assembly.js
+++ commands/general-assembly.js
@@ -12,34 +12,30 @@ module.exports = {
 
 
         if (isInitalized() === true) {
+            message.reply('Are you sure you want to call a Meeting?')
+
+            if (message.content === ['Yes' || 'yes' || 'Y' || 'y']) {
+                console.log('response works')
+            }
+            /*
+                        const VacantRole = message.guild.roles.cache.find(role => role.name === 'Vacant (General Assemby)')
+            
+                        console.log('yup');
+                        message.reply('Initializing a General Assembly..')
+                        //message.member.roles.add()
+                        message.guild.members.cache.forEach(
+                            member => member.roles.add(VacantRole)
+                        )
+                    }
+                    else {
+                        console.log('nope');
+                        message.reply('You need to run the setup command first!')
+                    }
+            
+            */
 
 
-            const VacantRole = message.guild.roles.cache.find(role => role.name === 'Vacant (General Assemby)')
-
-            console.log('yup');
-            message.reply('Initializing a General Assembly..')
-            //message.member.roles.add()
-            message.guild.members.cache.forEach(
-                member => member.roles.add(VacantRole)
-
-
-
-
-
-
-
-
-            )
         }
-        else {
-            console.log('nope');
-            message.reply('You need to run the setup command first!')
-        }
-
-
-
-
-
 
     }
 }
